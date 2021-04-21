10efabc2c671f818638b4f83713b2401f9b2e4b4
Kevin Tinnemans <kevintinnemans@upcmail.nl>
2021-04-21 11:59:31 +0200

    added test test

--- backend/profileservice/src/test/java/com/kwetter/profileservice/ProfileserviceApplicationTests.java
+++ backend/profileservice/src/test/java/com/kwetter/profileservice/ProfileserviceApplicationTests.java
@@ -2,12 +2,14 @@ package com.kwetter.profileservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ProfileserviceApplicationTests {
 
     @Test
     void contextLoads() {
+        Assert.isTrue(true);
     }
 
 }
