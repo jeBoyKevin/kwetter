8cd015e04c0840367e24d64bbb0ad94b77b37d8a
Kevin Tinnemans <kevintinnemans@upcmail.nl>
2021-04-21 12:59:44 +0200

    Does this work?

--- backend/profileservice/src/test/java/com/kwetter/profileservice/ProfileserviceApplicationTest.java
+++ backend/profileservice/src/test/java/com/kwetter/profileservice/ProfileserviceApplicationTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class ProfileserviceApplicationTests {
+class ProfileserviceApplicationTest {
 
     @Test
-    void contextLoads() {
-        Assert.isTrue(true);
+    void test() {
+        assertEquals("message","message");
     }
 
 }
