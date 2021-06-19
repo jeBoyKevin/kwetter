5b031edb954027c0d6879933e483798f3d9262b3
Kevin Tinnemans <kevintinnemans@upcmail.nl>
2021-06-19 11:08:02 +0200

    Accountservice to Kubernetes fix

--- backend/accountservice/src/main/java/com/kwetter/accountservice/WebConfig.java
+++ backend/accountservice/src/main/java/com/kwetter/accountservice/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements Filter,WebMvcConfigurer {
         HttpServletRequest request = (HttpServletRequest) req;
         System.out.println("WebConfig; "+request.getRequestURI());
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
